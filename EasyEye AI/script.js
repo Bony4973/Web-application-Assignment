0db50408500075e9ac0ae511120a018e445f4c38
Bony4973 <123311006+Bony4973@users.noreply.github.com>
2025-06-28 23:00:24 +0600

    Delete EasyEye AI directory

--- EasyEye AI/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.btn-primary').forEach(button => {
-  button.addEventListener('click', () => {
-    alert('Upload functionality not implemented in this mockup.');
-  });
-});
